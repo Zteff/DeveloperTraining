e259fbfb1d206e2ba156b6179d3883f1267874c2
Steffen Jørgensen <steffen.jorgensen@vippsmobilepay.com>
2023-02-06 15:06:57 +0100

    Add extension method

---
+++ Workshop1.DependencyInjection/ExtensionMethods/IocExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using Workshop1.DependencyInjection.Interfaces;
+using Workshop1.DependencyInjection.Operations;
+
+namespace Workshop1.DependencyInjection.ExtensionMethods;
+
+public static class IocExtensionMethods
+{
+    public static IServiceCollection AddOperations(this IServiceCollection services)
+    {
+        services.AddTransient<ITransientOperation, Operation>();
+        services.AddScoped<IScopedOperation, Operation>();
+        services.AddSingleton<ISingletonOperation, Operation>();
+        return services;
+    }
+}
