6673a88e19e0f0cb051566d0f3f211ba6f1df854
SteffenJørgensen <steffen.jorgensen@vippsmobilepay.com>
2023-03-02 14:38:25 +0100

    Added simple FooApender test

---
+++ Workshop2.UnitTesting.Tests/FooAppender_Should.cs
@@ -0,0 +1,18 @@
+﻿namespace Workshop2.UnitTesting.Tests;
+
+public class FooAppender_Should
+{
+    [Fact]
+    public void AppendFoo_When_AppendFooIsCalled()
+    {
+        // Arrange
+        var sut = new FooAppender();
+        var myString = "Some string";
+
+        // Act
+        var result = sut.AppendFoo(myString);
+
+        // Assert
+        Assert.Equal($"{myString}Foo", result);
+    }
+}
