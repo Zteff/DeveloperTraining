a75eec43f8c714aaf50b62eb94919fd7065d22c6
SteffenJørgensen <steffen.jorgensen@vippsmobilepay.com>
2023-03-02 15:01:02 +0100

    WIP

---
+++ Workshop2.UnitTesting.Tests/BarAppender_Should.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Workshop2.UnitTesting.Tests;
+
+public class BarAppender_Should
+{
+    [Fact]
+    public void AppendFoo_When_AppendBarIsCalled()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger>();
+        var sut = new BarAppender(loggerMock.Object);
+        var myString = "Some string";
+
+        // Act
+        var result = sut.AppendBar(myString);
+
+        // Assert
+        Assert.Equal($"{myString}Bar", result);
+    }
+    
+    [Fact]
+    public void CallLoggerOnce_When_AppendBarIsCalled()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger>();
+        var sut = new BarAppender(loggerMock.Object);
+        var myString = "Some string";
+
+        // Act
+        var result = sut.AppendBar(myString);
+
+        // Assert
+        loggerMock.Verify(x => x.Log(LogLevel.Information, $"Appending Bar to {myString}"));
+    }
+}
---
+++ Workshop2.UnitTesting/BarAppender.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Workshop2.UnitTesting;
+
+public class BarAppender
+{
+    private readonly ILogger _logger;
+
+    public BarAppender(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public string AppendBar(string stringToAppendTo)
+    {
+        _logger.Log(LogLevel.Information, $"Appending Bar to {stringToAppendTo}");
+        return $"{stringToAppendTo}Bar";
+    }
+}
