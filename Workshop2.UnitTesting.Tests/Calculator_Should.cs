09134ebcb76cdd2fb77bbf4d678e747044b6bc9c
Steffen Jørgensen <steffen.jorgensen@vippsmobilepay.com>
2023-03-06 15:17:51 +0100

    Calculator example

---
+++ Workshop2.UnitTesting.Tests/Calculator_Should.cs
@@ -0,0 +1,33 @@
+﻿namespace Workshop2.UnitTesting.Tests;
+
+public class Calculator_Should
+{
+    [Fact]
+    public void Return0_When_ArrayIsEmpty()
+    {
+        //Arrange
+        var sut = new Calculator();
+        var emptyArray = Array.Empty<int>();
+
+        //Act
+        var result = sut.Add(emptyArray);
+
+        //Assert
+        Assert.Equal(0, result);
+    }
+    
+    [Fact]
+    public void ReturnsNumber_When_ArrayOnlyContainsOneNumber()
+    {
+        //Arrange
+        var sut = new Calculator();
+        var number = 1;
+        var numberArray = new int[] { number };
+
+        //Act
+        var result = sut.Add(numberArray);
+
+        //Assert
+        Assert.Equal(number, result);
+    }
+}
---
+++ Workshop2.UnitTesting/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Workshop2.UnitTesting;
+
+public class Calculator
+{
+    public int Add(int[] numbersToAdd)
+    {
+        if (!numbersToAdd.Any())
+            return 0;
+        return numbersToAdd[0];
+    }
+}
