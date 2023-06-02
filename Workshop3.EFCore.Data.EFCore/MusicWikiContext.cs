ab2e72f9c596f479848342dc82e50ab056f1e440
Steffen <dk.steffen@gmail.com>
2023-06-02 09:36:07 +0200

    Add EF Core project

---
+++ Workshop3.EFCore.Data.EFCore/MusicWikiContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Workshop3.EFCore.Domain;
+
+namespace Workshop3.EFCore.Data.EFCore;
+
+public class MusicWikiContext : DbContext
+{
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+}
