6c608d5b50141c6b72cdf81fd275123c2668a2c5
Steffen <dk.steffen@gmail.com>
2023-06-02 13:24:30 +0200

    Initial setup of connectionstring from appsettings.json

--- Workshop3.EFCore.Data.EFCore/MusicWikiContext.cs
+++ Workshop3.EFCore.Data.EFCore/MusicWikiContext.cs
@@ -5,6 +5,18 @@ namespace Workshop3.EFCore.Data.EFCore;
 
 public class MusicWikiContext : DbContext
 {
+    private readonly string _connectionString;
+
+    public MusicWikiContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
     public DbSet<Artist> Artists { get; set; }
     public DbSet<Genre> Genres { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_connectionString);
+    }
 }
