5c0fa2631ab1e6d156405410aefac75395837ac8
SteffenJørgensen <steffen.jorgensen@vippsmobilepay.com>
2023-06-06 11:50:00 +0200

    Add GetGenres to context

--- Workshop3.EFCore.Data.EFCore/MusicWikiContext.cs
+++ Workshop3.EFCore.Data.EFCore/MusicWikiContext.cs
@@ -56,4 +56,71 @@ public class MusicWikiContext : DbContext
             }
         });
     }
+    
+    // private IEnumerable<Genre> GetGenres()
+    // {
+    //     return new[]
+    //     {
+    //         new Genre()
+    //         {
+    //             Name = "Trash Metal", Bands = new[]
+    //             {
+    //                 new Artist() { Name = "Metallica" },
+    //                 new Artist() { Name = "Megadeth" },
+    //                 new Artist() { Name = "Slayer" },
+    //                 new Artist() { Name = "Anthrax" },
+    //             }
+    //         },
+    //         new Genre()
+    //         {
+    //             Name = "Heavy Metal", Bands = new[]
+    //             {
+    //                 new Artist() { Name = "Iron Maiden" },
+    //                 new Artist() { Name = "Judas Priest" },
+    //                 new Artist() { Name = "Black Sabbath" },
+    //                 new Artist() { Name = "Accept" },
+    //             }
+    //         },
+    //         new Genre()
+    //         {
+    //             Name = "Death Metal", Bands = new[]
+    //             {
+    //                 new Artist() { Name = "Death" },
+    //                 new Artist() { Name = "Cannibal Corpse" },
+    //                 new Artist() { Name = "Morbid Angel" },
+    //                 new Artist() { Name = "Obituary" },
+    //             }
+    //         },
+    //         new Genre()
+    //         {
+    //             Name = "Black Metal", Bands = new[]
+    //             {
+    //                 new Artist() { Name = "Mayhem" },
+    //                 new Artist() { Name = "Darkthrone" },
+    //                 new Artist() { Name = "Emperor" },
+    //                 new Artist() { Name = "Burzum" },
+    //             }
+    //         },
+    //         new Genre()
+    //         {
+    //             Name = "Power Metal", Bands = new[]
+    //             {
+    //                 new Artist() { Name = "Helloween" },
+    //                 new Artist() { Name = "Blind Guardian" },
+    //                 new Artist() { Name = "Stratovarius" },
+    //                 new Artist() { Name = "Gamma Ray" },
+    //             }
+    //         },
+    //         new Genre()
+    //         {
+    //             Name = "Progressive Metal", Bands = new[]
+    //             {
+    //                 new Artist() { Name = "Dream Theater" },
+    //                 new Artist() { Name = "Symphony X" },
+    //                 new Artist() { Name = "Fates Warning" },
+    //                 new Artist() { Name = "Queensrÿche" },
+    //             }
+    //         },
+    //     };
+    // }
 }
