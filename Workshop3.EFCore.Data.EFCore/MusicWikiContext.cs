6c922047ce0217af7cad9479b91ce0b865942cb9
SteffenJørgensen <steffen.jorgensen@vippsmobilepay.com>
2023-06-06 09:32:50 +0200

    Add albums using seed

--- Workshop3.EFCore.Data.EFCore/MusicWikiContext.cs
+++ Workshop3.EFCore.Data.EFCore/MusicWikiContext.cs
@@ -26,10 +26,34 @@ public class MusicWikiContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Artist>().HasData(new[]
         {
-            new Artist() { Id=1,Name = "Metallica" },
+            new Artist() { Id=1,Name = "Metallica"},
             new Artist() { Id=2,Name = "Megadeth" },
             new Artist() { Id=3,Name = "Slayer" },
             new Artist() { Id=4,Name = "Anthrax" }
+        });
+        modelBuilder.Entity<Album>().HasData(new[]
+        {
+            new Album()
+            {
+                Id=3,
+                Title = "Master of Puppets",
+                ReleaseDate = new DateTime(1986, 3, 3),
+                ArtistId = 1
+            },
+            new Album()
+            {
+                Id=2,
+                Title = "Ride the Lightning",
+                ReleaseDate = new DateTime(1984, 7, 27),
+                ArtistId = 1
+            },
+            new Album()
+            {
+                Id=1,
+                Title = "Kill 'Em All",
+                ReleaseDate = new DateTime(1983, 7, 25),
+                ArtistId = 1
+            }
         });
     }
 }
