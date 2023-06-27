a1c8f51be8dce5da318d76d9eac8308e0cbd79eb
SteffenJørgensen <steffen.jorgensen@vippsmobilepay.com>
2023-06-27 13:37:35 +0200

    AddGenre migration

---
+++ Workshop3.EFCore.Data.EFCore/Migrations/20230627113604_AddGenre.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workshop3.EFCore.Data.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GenreId",
+                table: "Artists",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "GenreId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "GenreId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "GenreId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "GenreId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artists_GenreId",
+                table: "Artists",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Artists_Genre_GenreId",
+                table: "Artists",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Artists_Genre_GenreId",
+                table: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Artists_GenreId",
+                table: "Artists");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Artists");
+        }
+    }
+}
